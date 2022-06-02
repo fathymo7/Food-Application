96ac0ef88ae12930c70641267136c4c699d5cd35
fathymo7 <FATHYmohsen47@gmail.com>
2022-06-02 21:02:27 +0200

    Update

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfItems = cartCtx.items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,23 +1,20 @@
-import React from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = props => {
+const CartProvider = (props) => {
+  const addItemToCart = (item) => {};
 
-    const addItemToCart = item => {}
+  const removeItemFromCart = (id) => {};
 
-    const removeItemFromCart = id => {}
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCart,
+    removeItem: removeItemFromCart,
+  };
 
-    const cartContext = {
-        item: [],
-        totalAmount: 0,
-        addItem: ,
-        removeItem: 
-    };
-
-    return 
-        <CartContext.Provider>
-{props.children}
-        </CartContext.Provider>
-    
-}
+  return;
+  <CartContext.Provider value={cartContext}>
+    {props.children}
+  </CartContext.Provider>;
+};
 export default CartProvider;
