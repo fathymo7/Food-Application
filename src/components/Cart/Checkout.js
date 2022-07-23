7087f708a31a7973c7dac8b2acd58f2039af6549
fathymo7 <FATHYmohsen47@gmail.com>
2022-07-23 21:20:12 +0200

    Update

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,30 +1,47 @@
-import React from "react";
 import classes from "./Checkout.module.css";
+import { useRef } from "react";
 
 const Checkout = (props) => {
-  const confirmHandler =   (event) => {
+  const nameInputRef = useRef();
+  const mobileInputRef = useRef();
+  const cityInputRef = useRef();
+  const streetInputRef = useRef();
+
+  const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredMobile = mobileInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
   };
 
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor="num">Number</label>
-        <input type="number" id="num" />
+        <label htmlFor="mobile">Mobile Number</label>
+        <input type="text" id="mobile" ref={mobileInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor="street">Address</label>
-        <input type="text" id="street" />
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.control}>
+        <label htmlFor="Street">Address</label>
+        <input type="text" id="Street" ref={streetInputRef} />
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
+
 export default Checkout;
